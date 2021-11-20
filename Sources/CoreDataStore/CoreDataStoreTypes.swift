0cd3f8d912b012a88c682a18ca666a8474b901ef
Kelvin Kosbab <kelvin@kosbab.com>
2021-11-19 17:10:24 -0800

    Remove unneeded types.

--- Sources/CoreDataStore/CoreDataStoreTypes.swift
+++ Sources/CoreDataStore/CoreDataStoreTypes.swift
@@ -36,34 +36,6 @@ public extension Set where Element : StructConvertable {
 
 // MARK: - CoreDataAssociated
 
-public protocol CoreDataAssociated : Hashable, Codable {
+public protocol CoreDataAssociated : Hashable {
     associatedtype ManagedObject : StructConvertable
-}
-
-// MARK: - IdentifiableObject
-
-/// Use the Identifiable protocol to provide a stable notion of identity to a class or value type. For example, you could define a User type
-/// with an id property that is stable across your app and your app’s database storage. You could use the id property to identify a
-/// particular user even if other data fields change, such as the user’s name.
-///
-/// Identifiable leaves the duration and scope of the identity unspecified. Identities could be any of the following:
-/// * Guaranteed always unique (e.g. UUIDs).
-/// * Unique for the lifetime of an object (e.g. object identifiers).
-///
-/// It is up to both the conformer and the receiver of the protocol to document the nature of the identity.
-public protocol IdentifiableObject {
-    var identifier: String { get }
-}
-
-// MARK: - IdentifiableParent
-
-/// Use the Identifiable protocol to provide a stable notion of identity to a class or value type's parent object.
-///
-/// Identifiable leaves the duration and scope of the identity unspecified. Identities could be any of the following:
-/// * Guaranteed always unique (e.g. UUIDs).
-/// * Unique for the lifetime of an object (e.g. object identifiers).
-///
-/// It is up to both the conformer and the receiver of the protocol to document the nature of the identity.
-public protocol IdentifiableParent {
-    var parentIdentifier: String { get }
 }
